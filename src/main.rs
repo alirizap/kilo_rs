116eb2fef74b4c984141b6855d8154f9722b9cb9
Alireza Pishdar <pishdaralirza@gmail.com>
2025-04-12 11:21:44 +0430

    init editor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use crossterm::terminal::size;
+
+struct EditorConfig {
+    screen_rows: u16,
+    screen_cols: u16,
+}
+
+struct Editor {
+    config: EditorConfig,
+}
+
+impl Editor {
+    fn new(screen_rows: u16, screen_cols: u16) -> Self {
+        Self {
+            config: EditorConfig {
+                screen_rows,
+                screen_cols,
+            },
+        }
+    }
+
+    fn run(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let (screen_cols, screen_rows) = size()?;
+    let mut editor = Editor::new(screen_rows, screen_cols);
+    editor.run()?;
+
+    Ok(())
 }
