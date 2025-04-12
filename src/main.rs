82f36b163140fa411789f4a9f523a3796b3f8e53
Alireza Pishdar <pishdaralirza@gmail.com>
2025-04-12 11:25:01 +0430

    enable raw mode and handling errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use std::io::stdout;
+
 use anyhow::Result;
-use crossterm::terminal::size;
+use crossterm::{
+    execute,
+    terminal::{
+        disable_raw_mode, enable_raw_mode, size, EnterAlternateScreen, LeaveAlternateScreen,
+    },
+};
 
 struct EditorConfig {
     screen_rows: u16,
@@ -27,8 +34,14 @@ impl Editor {
 
 fn main() -> Result<()> {
     let (screen_cols, screen_rows) = size()?;
+    execute!(stdout(), EnterAlternateScreen)?;
+    enable_raw_mode()?;
     let mut editor = Editor::new(screen_rows, screen_cols);
-    editor.run()?;
+    if let Err(e) = editor.run() {
+        eprint!("{e}");
+        disable_raw_mode().unwrap();
+        execute!(stdout(), LeaveAlternateScreen).unwrap();
+    }
 
     Ok(())
 }
